a52759183b189f7ba23e7b7dfbcb65a3a6632a35
Bhaveshthakur-348 <bhaveshthakur348@gmail.com>
2021-02-24 23:00:34 +0530

    func

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [showCont, setCont] = React.useState(false);
+  let showPara = () => {
+    setCont(true);
+  };
+
   return (
     <div id="main">
       // Do not alter the main div
+      <button id="click" onClick={showPara}></button>
+      {showCont ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
     </div>
   );
 }
-
 
 export default App;
